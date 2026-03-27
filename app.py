cb81bf4075fc5bf8f6de8b84603b13635b68359b
george-sx <stewgeorge3@gmail.com>
2026-03-26 20:43:44 -0400

    Use bootstrap-flask template and add basic login screen

--- app.py
+++ app.py
@@ -1,19 +1,49 @@
-"""
-    HOW TO RUN:
-
-    1) Follow Flask installation guide, ensure virtual environment is active if you are using one
-    (You can also use 'pip install -r requirements.txt' to download all dependencies from the requirements.txt file)
-
-    2) Run the following command inside the project folder: 'flask run --debug'
-    Note: Running 'flask run' automatically looks for app.py and runs it, to run another module/page make sure to specify its name using --app
-
-    To edit HTML, make/modify an HTML template inside templates then use the Flask render_template() method to render it (unless you plan on just hard-coding HTML in directly)
-"""
-
-from flask import Flask, render_template
+from enum import Enum
+from flask import Flask, render_template, request, flash, redirect, url_for
+from markupsafe import Markup
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms.validators import DataRequired, Length, Regexp
+from wtforms.fields import *
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap5, SwitchField
 
 app = Flask(__name__)
+app.secret_key = 'dev'
 
-@app.route("/")
-def hello_world():
-    return render_template('app.html');
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+
+# serve locally for faster and offline development
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+# set default button sytle and size, will be overwritten by macro parameters
+app.config['BOOTSTRAP_BTN_STYLE'] = 'primary'
+app.config['BOOTSTRAP_BTN_SIZE'] = 'sm'
+
+# set default icon title of table actions
+app.config['BOOTSTRAP_TABLE_VIEW_TITLE'] = 'Read'
+app.config['BOOTSTRAP_TABLE_EDIT_TITLE'] = 'Update'
+app.config['BOOTSTRAP_TABLE_DELETE_TITLE'] = 'Remove'
+app.config['BOOTSTRAP_TABLE_NEW_TITLE'] = 'Create'
+
+bootstrap = Bootstrap5(app)
+db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 150)])
+    remember = BooleanField('Remember me')
+    submit = SubmitField()
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', form = LoginForm())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
